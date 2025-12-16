7e773d269125f581b3b30bf6cb142ddd80fcce82
micheleferri01 <micheleferri095@gmail.com>
2025-12-16 15:11:53 +0100

    variabili globali

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Variabili globali
+const pricePerK = 0.21;
+let message;
+
+// Raccolta dati
+
+// Chiedo all'utente il numero di chilometri che vuole percorrere (via prompt)
+const userKil = parseInt(prompt("Inserire numero di chilometri che si vogliono percorrere."));
+
+// Chiedo all'utente la sua età (via prompt)
+const userAge = parseInt(prompt("Inserire età."));
+
+// Elaborazione
